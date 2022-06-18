e1ede635eae86864558230d54e3c368afdf75c3e
Samuel Varghese <gamingsam77@gmail.com>
2022-06-19 00:38:20 +0100

    Added basic html as strings in main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,18 @@
+// read mark down files in directory under assets probably 
+ 
+pub const  HEADER: &str = r#"<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>"#;
+
+pub const BODY: &str = r#"<body></body>"#;
+
+pub const FOOTER: &str = r#"</html>"#;
+
 fn main() {
     println!("Hello, world!");
 }
